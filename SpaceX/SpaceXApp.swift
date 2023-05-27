bdddaf6f738b8799712e1cd64e76acf0b21aec00
Ramdhas <ramdhas.m@nexergroup.com>
2023-05-27 10:55:32 +0200

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Ramdhas on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
